b1d1089023367104ca671b8321aa1c740c86ba48
Utsav Das <utsavdas@Utsavs-MacBook-Pro.local>
2021-06-22 00:57:41 -0400

    update

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
-document.body.innerHTML = document.body.innerHTML.replace(/Mr. Trump/g, "Crime master Gogo");
+document.body.innerHTML = document.body.innerHTML.replace(/content a/g, "content b"); // replace content a and content b with required text
 
-document.body.innerHTML = document.body.innerHTML.replace(/Donald Trump/g, "Crime master Gogo");
+document.body.innerHTML = document.body.innerHTML.replace(/content a/g, "content b");
 
-document.body.innerHTML = document.body.innerHTML.replace(/President Trump/g, "Crime master Gogo");
+document.body.innerHTML = document.body.innerHTML.replace(/content a/g, "content b");
 
-document.body.innerHTML = document.body.innerHTML.replace(/Trump/g, "Crime master Gogo");
+document.body.innerHTML = document.body.innerHTML.replace(/content a/g, "content b");
